637a112a35c90673d991e642f03ea0448dc6fca0
Ekapop <eploentham@gmail.com>
2017-03-04 22:32:50 +0700

    600304
    oilpc

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer class="color-footer">
+    <div class="widget-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-md-4">
+              <div class="about-widget">
+                <div class="widget-title">
+                  <img src="images/logo.png" alt="">
+                </div>
+                <p>Praesent sed congue ipsum. Nullam tempus ornare est, non aliquet velit tincidunt elementum. Nulla at risus ut felis eleifend facilisis at eget diam. Vestibulum quis accumsan velit. Mauris vitae augue at leo mattis sollicitudin dignissim in justo.
+                <br>
+                <br>
+                Nulla non lacinia. Integer est lacus, ultricies sed feugiat id, maximus nec.</p>
+              </div><!-- / about-widget -->
+            </div><!-- / col-sm-6 col-md-3 -->
+            <!-- / first widget -->
+
+            <div class="col-sm-6 col-md-4">
+              <div class="widget-title">
+                <h4>Newsletter</h4>
+              </div>
+              <div id="newsletter" class="newsletter-widget">
+                <div class="input">
+                  <input type="text" class="form-control" placeholder="Email">
+                  <span class="input-btn">
+                    <button class="btn btn-md btn-success-filled btn-newsletter" type="button">Subscribe to Newsletter</button>
+                  </span>
+                </div><!-- /input-group -->
+              </div><!-- / newsletter-widget -->
+            </div><!-- / col-sm-6 col-md-3 -->
+            <!-- / third widget -->
+
+            <div class="col-sm-6 col-md-4">
+              <div class="widget-title">
+                <h4>Contact</h4>
+              </div>
+              <div class="contact-widget">
+                <div class="info">
+                  <p><i class="lnr lnr-map-marker"></i><span>Ram intra, Soi ramintra14, Bangkok Thailand.</span></p>
+                </div>
+                <div class="info">
+                  <a href="tel:+0123456789"><i class="lnr lnr-phone-handset"></i><span>+6694 851 7557</span></a>
+                </div>
+                <div class="info">
+                  <a href="mailto:hello@yoursite.com"><i class="lnr lnr-envelope"></i><span>ekapop@nakoyagarden.com</span></a>
+                </div>
+                <div class="info">
+                  <p class="social pull-left">
+                    <a class="no-margin" href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                  </p>
+                </div>
+              </div><!-- / contact-widget -->
+            </div><!-- / col-sm-6 col-md-3 -->
+            <!-- / fourth widget -->
+          </div><!-- / row -->
+        </div><!-- / container -->
+      </div><!-- / widget-area -->
+        <div class="container">  <p class="footer-info text-center">© 2017 - <strong>NakoyaSoft</strong> - Bootstrap UI Kit. All Rights Reserved.</p>
+    </div><!-- / container -->
+</footer><!-- / color-footer with widgets -->
