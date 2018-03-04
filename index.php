6a668ab692e0fc7404a43e043542603931ce2a28
Ekapop <eploentham@gmail.com>
2018-03-04 21:59:55 +0700

    1
    1

--- index.php
+++ index.php
@@ -82,6 +82,17 @@ if(isset($_SESSION["status_regis"])){
             }
             
         </style>
+            <!-- ClickDesk Live Chat Service for websites -->
+    <script type='text/javascript'>
+        var _glc =_glc || []; _glc.push('all_ag9zfmNsaWNrZGVza2NoYXRyEgsSBXVzZXJzGICA4Ojs8sQLDA');
+        var glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' : 
+        'http://my.clickdesk.com/clickdesk-ui/browser/');
+        var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
+        var glcspt = document.createElement('script'); glcspt.type = 'text/javascript'; 
+        glcspt.async = true; glcspt.src = glcpath + 'livechat-new.js';
+        var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
+    </script>
+    <!-- End of ClickDesk -->
 </head>
 <body class="header-fixed" id="body1">
     <div class="wrapper page-option-v1">
